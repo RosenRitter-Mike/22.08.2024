0f57661264182b238577349eb7cce963d7695c85
RosenRitter-Mike <mike88roze@gmail.com>
2024-08-23 22:37:34 +0300

    draft 1

--- calculator.py
+++ calculator.py
@@ -1,3 +1,4 @@
+import math
 
 def add(a, b):
     return a + b
@@ -22,12 +23,35 @@ def power(a: int, b: int) -> float:
             raise ZeroDivisionError("Cannot divide by zero!");
         else:
             num: float = 1 / a;
+            b *= -1;
     else:
         num = a;
+
     res = num;
-    for _ in range(b):
+    for _ in range(b-1):
        res *= num;
+
     return res;
+
+def sqrt(a: int) -> float:
+    return math.sqrt(a);
+
+def is_primary(a: int) -> bool:
+    if a < 2:
+        return False;
+    for i in range(2, a):
+        if not a%i:
+            return False;
+    return True;
+
+def factorial(a: int) -> int:
+    if a < 0:
+        raise ValueError("factorial not defined for negative values");
+    elif a == 0:
+        return 1;
+    else:
+        return a*factorial(a-1);
+
 # testing:
 # check all cases
 # auto keep functionality working
--- test_calculator.py
+++ test_calculator.py
@@ -87,4 +87,206 @@ def test_calculator_div_zero_error_phase2():
 
     assert str(ex.value) == "Cannot divide by zero!"
 
+def test_calculator_pow_small():
+    x: int = 2;
+    y: int = 10;
+    expected: float = 1024;
+
+    # Act
+    actual: float = calculator.power(x, y);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_pow_16():
+    x: int = 2;
+    y: int = 4;
+    expected: float = 16;
+
+    # Act
+    actual: float = calculator.power(x, y);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_pow_9():
+    x: int = 3;
+    y: int = 2;
+    expected: float = 9;
+
+    # Act
+    actual: float = calculator.power(x, y);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_pow_zero():
+    x: int = 9;
+    y: int = 0;
+    expected: float = 1;
+
+    # Act
+    actual: float = calculator.power(x, y);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_pow_neg():
+    x: int = 2;
+    y: int = -2;
+    expected: float = 0.25;
+
+    # Act
+    actual: float = calculator.power(x, y);
+
+    # Assert
+    assert actual == expected;
+
+
+def test_calculator_pow_neg_div_zero_error():
+    x: int = 0
+    y: int = -2
+
+    # Act
+    try:
+        actual: int = calculator.power(x, y)
+        assert False  # fail the test
+    except ZeroDivisionError as e:
+        assert True  # pass the test
+
+    with pytest.raises(ZeroDivisionError) as ex:
+        actual: int = calculator.power(x, y)
+
+    assert str(ex.value) == "Cannot divide by zero!"
+
+def test_calculator_sqrt_positive():
+    x: int = 25;
+
+    expected: float = 5;
+
+    # Act
+    actual: float = calculator.sqrt(x);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_sqrt_negative():
+    x: int = -5;
+
+    try:
+        actual: float = calculator.sqrt(x)
+        assert False  # fail the test
+    except ValueError as e:
+        assert True  # pass the test
+
+    with pytest.raises(ValueError) as ex:
+        actual = calculator.sqrt(x)
+
+    assert str(ex.value) == "math domain error"
+
+def test_calculator_is_prime_i():
+    x: int = 1;
+
+    assert not calculator.is_primary(x);
+
+
+def test_calculator_is_prime_j():
+    x: int = 2;
+
+    assert calculator.is_primary(x);
+
+def test_calculator_is_prime_k():
+    x: int = 16;
+
+    assert not calculator.is_primary(x);
+
+def test_calculator_is_prime_l():
+    x: int = -3;
+
+    assert not calculator.is_primary(x);
+
+def test_calculator_is_prime_m():
+    x: int = 0;
+
+    assert not calculator.is_primary(x);
+
+def test_calculator_factorial_n():
+    x: int = 4;
+
+    expected: int = 24;
+
+    # Act
+    actual: int = calculator.factorial(x);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_factorial_o():
+    x: int = 0;
+
+    expected: int = 1;
+
+    # Act
+    actual: int = calculator.factorial(x);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_factorial_p():
+    x: int = 1;
+
+    expected: int = 1;
+
+    # Act
+    actual: int = calculator.factorial(x);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_factorial_q():
+    x: int = 5;
+
+    expected: int = 120;
+
+    # Act
+    actual: int = calculator.factorial(x);
+
+    # Assert
+    assert actual == expected;
+
+def test_calculator_factorial_r():
+    x: int = -3;
+
+    try:
+        actual: float = calculator.factorial(x)
+        assert False  # fail the test
+    except ValueError as e:
+        assert True  # pass the test
+
+    with pytest.raises(ValueError) as ex:
+        actual = calculator.factorial(x)
+
+    assert str(ex.value) == "factorial not defined for negative values"
+
+test_calculator_pow_small()
+test_calculator_pow_16()
+test_calculator_pow_9()
+test_calculator_pow_zero()
+test_calculator_pow_neg()
+test_calculator_pow_neg_div_zero_error()
+test_calculator_sqrt_positive()
+test_calculator_sqrt_negative()
+test_calculator_is_prime_i()
+test_calculator_is_prime_j()
+test_calculator_is_prime_k()
+test_calculator_is_prime_l()
+test_calculator_is_prime_m()
+test_calculator_factorial_n()
+test_calculator_factorial_o()
+test_calculator_factorial_p()
+test_calculator_factorial_q()
+test_calculator_factorial_r()
+
+
+
 
